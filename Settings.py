d26ba669782568889572e64d43798e6001ab237a
sangxiaoqian <77234010+sangxiaoqian@users.noreply.github.com>
2021-01-10 21:38:39 +0800

    Update Settings.py

--- Settings.py
+++ Settings.py
@@ -1,17 +1,17 @@
 # 以下区域为需要编辑的区域，请填写，其他区域勿动 #
 # ￥ 邮件服务 ￥
-email_address = ''  # 填写邮箱地址，示例  email_address = 'sample@qq.com'
-email_password = ''  # 发送方邮箱密码。QQ邮箱，新浪邮箱请填写授权码
+email_address = '1670794372@qq.com'  # 填写邮箱地址，示例  email_address = 'sample@qq.com'
+email_password = 'rubfmbnffzordabc'  # 发送方邮箱密码。QQ邮箱，新浪邮箱请填写授权码
 email_server = 'smtp.qq.com'  # 邮件服务器地址，在网上查找 示例（QQ邮件服务器）  email_server = 'smtp.qq.com'
 email_port = 465  # 邮件服务器端口号，在网上查找 示例（QQ邮件服务器）  email_port = 465
 # ￥ 一站式学生认证 ￥
-stu_no = ''  # 学号 示例 stu_no = '18069100000'
-stu_pass = ''  # 统一登录密码 示例 stu_pass = 'abcdefgh'
+stu_no = '20069100143'  # 学号 示例 stu_no = '18069100000'
+stu_pass = 'sxq12345'  # 统一登录密码 示例 stu_pass = 'abcdefgh'
 # ￥ 设置地点 ￥
-province = u''  # 所在省 示例 province = u'陕西省'
-city = u''  # 所在市 示例 city = u'西安市'
-area = u''  # 所在区 示例 area = u'长安区'
-address = u''  # 具体地点
+province = u'甘肃省'  # 所在省 示例 province = u'陕西省'
+city = u'兰州市'  # 所在市 示例 city = u'西安市'
+area = u'城关区'  # 所在区 示例 area = u'长安区'
+address = u'兰泰家属院'  # 具体地点
 # ￥ 填写设置 ￥
 DailyUp = False  # 晨午晚检
 DailyReport = True  # 疫情通
